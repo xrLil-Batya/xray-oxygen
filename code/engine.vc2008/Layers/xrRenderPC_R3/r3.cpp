7a2fcbcc4da8df4c19794d81a948b1b15bc0017a
mrmnwar <mrmnwar@users.noreply.github.com>
2017-06-20 21:03:52 +0000

    r3.cpp edited online with Bitbucket

--- code/engine.vc2008/Layers/xrRenderPC_R3/r3.cpp
+++ code/engine.vc2008/Layers/xrRenderPC_R3/r3.cpp
@@ -1318,7 +1318,7 @@ HRESULT	CRender::shader_compile			(
 	HRESULT		_result = E_FAIL;
 
 	string_path	folder_name, folder;
-	xr_strcpy		( folder, "r3\\objects\\r3\\" );
+	xr_strcpy		( folder, "objects\\r3\\" );
 	xr_strcat		( folder, name );
 	xr_strcat		( folder, "." );
 
