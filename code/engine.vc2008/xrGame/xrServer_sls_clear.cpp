df235bf137bd3728f23ab7e44ffdd1c33354d307
ForserX <sv3nk@yandex.ru>
2017-07-27 15:38:53 +0300

    * FIX: incorrect format string

--- code/engine.vc2008/xrGame/xrServer_sls_clear.cpp
+++ code/engine.vc2008/xrGame/xrServer_sls_clear.cpp
@@ -18,7 +18,7 @@ void xrServer::Perform_destroy	(CSE_Abstract* object, u32 mode)
 	while (!object->children.empty()) 
 	{
 		CSE_Abstract		*child = game->get_entity_from_eid(object->children.back());
-		R_ASSERT4			(child, "child registered but not found [%d]", object->children.back(), object->name());
+		R_ASSERT4		(child, "child registered but not found [%d] [%s]", object->children.back(), object->name());
 		Perform_reject		(child,object,2*NET_Latency);
 #if defined(DEBUG) && defined(SLOW_VERIFY_ENTITIES)
 		verify_entities			();
