1afa51bdfa197470e4c81c9bac34d1efb1ed09a0
ForserX <sv3nk@yandex.ru>
2017-08-04 20:13:09 +0300

    * FIX: quick changing of target slot restarts animation of hiding (Sin!)

--- code/engine.vc2008/xrGame/CustomDetector.cpp
+++ code/engine.vc2008/xrGame/CustomDetector.cpp
@@ -123,7 +123,7 @@ void CCustomDetector::ToggleDetector(bool bFastMode)
 void CCustomDetector::OnStateSwitch(u32 S)
 {
 	inherited::OnStateSwitch(S);
-
+	u32 oldState = GetState();
 	switch(S)
 	{
 	case eShowing:
@@ -135,9 +135,13 @@ void CCustomDetector::OnStateSwitch(u32 S)
 		}break;
 	case eHiding:
 		{
-			m_sounds.PlaySound			("sndHide", Fvector().set(0,0,0), this, true, false);
-			PlayHUDMotion				(m_bFastAnimMode?"anm_hide_fast":"anm_hide", FALSE/*TRUE*/, this, GetState());
-			SetPending					(TRUE);
+			// [fixed] quick changing of target slot restarts animation of hiding
+			if (oldState != eHiding)
+			{
+				m_sounds.PlaySound			("sndHide", Fvector().set(0,0,0), this, true, false);
+				PlayHUDMotion				(m_bFastAnimMode?"anm_hide_fast":"anm_hide", FALSE/*TRUE*/, this, GetState());
+				SetPending					(TRUE);
+			}
 		}break;
 	case eIdle:
 		{
