dd7cad0ef549c34fe2defc61ea3aa9da09c973a4
ForserX <sv3nk@yandex.ru>
2017-08-11 19:08:27 +0300

    * LuaJIT: Limit mcode alloc probing, depending on the available pool size.

--- code/engine.vc2008/xrScripts/luajit/lj_mcode.c
+++ code/engine.vc2008/xrScripts/luajit/lj_mcode.c
@@ -230,7 +230,7 @@ static void *mcode_alloc(jit_State *J, size_t sz)
   /* First try a contiguous area below the last one. */
   uintptr_t hint = J->mcarea ? (uintptr_t)J->mcarea - sz : 0;
   int i;
-  for (i = 0; i < 32; i++) {  /* 32 attempts ought to be enough ... */
+  for (i = 0; i < LJ_TARGET_JUMPRANGE; i++) {  /* 32 attempts ought to be enough ... */
     if (mcode_validptr(hint)) {
       void *p = mcode_alloc_at(J, hint, sz, MCPROT_GEN);
 
