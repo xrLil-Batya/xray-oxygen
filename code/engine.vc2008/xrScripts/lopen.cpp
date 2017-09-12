c88124fe1b4b6c23b1075cfc26dfe90551667418
sv3nk <sv3nk@yandex.ru>
2017-09-12 20:07:23 +0300

    * ADD: Added LFS (#16)

--- code/engine.vc2008/xrScripts/lopen.cpp
+++ code/engine.vc2008/xrScripts/lopen.cpp
@@ -10,11 +10,13 @@ extern "C"
 {
 	extern const struct luaL_Reg *funcs;
 	int luaopen_LuaXML_lib(lua_State* L);
+	int luaopen_lfs(lua_State *L);
 }
 
 /* SCRIPT_API */ int lopen::marshal(lua_State *L)
 {
 	luaL_openlib(L, "mrsh", funcs, 0);
 	luaopen_LuaXML_lib(L); // Чтоб не пересобирать xrGame 
+	luaopen_lfs(L);
 	return 1;
 }
