e9a1191ddce75a711474c06825548bc8720f2fe1
sv3nk <sv3nk@yandex.ru>
2017-09-27 16:19:25 +0300

    * ADD: Added Release_AI32 configuration (not use AVX)

--- code/engine.vc2008/xrCore/_math.cpp
+++ code/engine.vc2008/xrCore/_math.cpp
@@ -251,7 +251,7 @@ void _initialize_cpu	(void)
     if (CPU::ID.hasFeature(CpuFeature::Sse42))  xr_strcat(features,", SSE4.2");
 	if (CPU::ID.hasFeature(CpuFeature::HT))     xr_strcat(features, ", HTT");
 	if (CPU::ID.hasFeature(CpuFeature::AVX))     xr_strcat(features, ", AVX");
-#ifdef _M_X64
+#if defined(_M_X64) && !defined(AI32_USE)
 	else Debug.do_exit("X-Ray x64 using AVX anyway!");
 #endif
 	Msg("* CPU features: %s" , features );
