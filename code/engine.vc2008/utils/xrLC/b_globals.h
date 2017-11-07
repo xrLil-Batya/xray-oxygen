70300f1b9ce38ca945b671311f7acbd43b373f10
StalkerEz <dnepar2010@yandex.ru>
2017-11-07 21:03:38 +0300

    * ADD: Added key "-notessellation" for xrLC

--- code/engine.vc2008/utils/xrLC/b_globals.h
+++ code/engine.vc2008/utils/xrLC/b_globals.h
@@ -27,6 +27,7 @@ struct SBuildOptions
 	bool b_noise;
 	bool b_net_light;
 	bool b_skipinvalid;
+	bool b_notessellation;
 	u32	 Priority;
 	SBuildOptions() :b_radiosity(FALSE), b_noise(FALSE), b_net_light(FALSE)
 	{
