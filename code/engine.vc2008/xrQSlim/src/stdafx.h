ff1dfa5006362b7bd97131732bdb27a2acb50896
sv3nk <sv3nk@yandex.ru>
2017-11-10 16:48:56 +0300

    * MOVE: utils moved to 'code/' * MOVE: ETools and xrQSlim moved to 'engine.vc2008/'

---
+++ code/engine.vc2008/xrQSlim/src/stdafx.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../../xrCore/xrCore.h"
+
+/************************************************************************
+
+Standard base include file for the MIX library.  This defines various
+common stuff that is used elsewhere.
+
+Copyright (C) 1998 Michael Garland.  See "COPYING.txt" for details.
+
+$Id: stdmix.h,v 1.21.2.1 2002/01/31 18:38:37 garland Exp $
+
+************************************************************************/
+
+#if defined(_DEBUG) && defined(_MSC_VER)
+#pragma warning (disable : 4786)
+#endif
+
+inline bool streq(const char *a, const char *b) { return !xr_strcmp(a,b); }
