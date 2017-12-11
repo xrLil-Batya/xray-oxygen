6e369c8ddbe3df789eb825d0815512260e802eaf
sv3nk <sv3nk@yandex.ru>
2017-12-11 23:11:49 +0300

    * DEL: Removed unnecessary method from the interface IApplicationRender. * EDIT: Minor update of the abstract class(interface) IApplicationRender.

--- code/engine.vc2008/Include/xrRender/ApplicationRender.h
+++ code/engine.vc2008/Include/xrRender/ApplicationRender.h
@@ -1,5 +1,3 @@
-#ifndef ApplicationRender_included
-#define ApplicationRender_included
 #pragma once
 
 class CApplication;
@@ -7,15 +5,11 @@ class CApplication;
 class IApplicationRender
 {
 public:
-	virtual ~IApplicationRender() {;}
+	virtual ~IApplicationRender() = default;
 	virtual void Copy(IApplicationRender &_in) = 0;
 
 	virtual void LoadBegin() = 0;
 	virtual void destroy_loading_shaders() = 0;
 	virtual void setLevelLogo(LPCSTR pszLogoName) = 0;
 	virtual void load_draw_internal(CApplication &owner) = 0;
-	//	?????
-	virtual void KillHW() = 0;
 };
-
-#endif	//	ApplicationRender_included
