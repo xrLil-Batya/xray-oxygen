33d9f4205cd3a89170078dd18bd5dc479179cb43
Alex <35600636+v2v3v4@users.noreply.github.com>
2018-01-21 07:09:35 +0300

    New position of fire for EacLookAt

--- code/engine.vc2008/xrGame/Actor_Weapon.cpp
+++ code/engine.vc2008/xrGame/Actor_Weapon.cpp
@@ -62,16 +62,23 @@ float CActor::GetWeaponAccuracy() const
 
 void CActor::g_fireParams	(const CHudItem* pHudItem, Fvector &fire_pos, Fvector &fire_dir)
 {
-    	fire_pos		= Cameras().Position();
-	fire_dir		= Cameras().Direction();
-
-	const CMissile	*pMissile = smart_cast <const CMissile*> (pHudItem);
+	CWeapon				*weapon = smart_cast<CWeapon*>(inventory().ActiveItem());
+	if(weapon)
+	{
+	if(eacFirstEye == cam_active)
+	fire_pos = Cameras().Position();
+	else
+	fire_pos		= weapon->get_LastFP();
+	}
+	else{
+const CMissile	*pMissile = smart_cast <const CMissile*> (pHudItem);
 	if (pMissile)
 	{
-		Fvector offset;
-		XFORM().transform_dir(offset, pMissile->throw_point_offset());
-		fire_pos.add(offset);
+	fire_pos = Actor()->Position();
+	fire_pos.y += 1.24f;
 	}
+	}
+	fire_dir		= Cameras().Direction();
 }
 
 void CActor::g_WeaponBones	(int &L, int &R1, int &R2)
