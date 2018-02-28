92e440039b0c2949b1aabedbc0d7a404efb32f52
sv3nk <sv3nk@yandex.ru>
2018-03-01 00:11:04 +0300

    * ADD: (Actor Editor) LoadDialog is working!

--- code/editors/xrMeshEditor/MainForm.h
+++ code/editors/xrMeshEditor/MainForm.h
@@ -35,6 +35,14 @@ namespace ECore {
 		}
 	private: System::Windows::Forms::GroupBox^  groupBox1;
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  filesToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  meshToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  loadToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  saveToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  exportToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  toolsToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  helpToolStripMenuItem;
 
 
 
@@ -58,22 +66,95 @@ namespace ECore {
 		{
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->filesToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->meshToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->loadToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->saveToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exportToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->helpToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// groupBox1
 			// 
 			this->groupBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
 				static_cast<System::Int32>(static_cast<System::Byte>(70)));
-			this->groupBox1->Location = System::Drawing::Point(748, -1);
+			this->groupBox1->Location = System::Drawing::Point(748, 27);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(200, 535);
+			this->groupBox1->Size = System::Drawing::Size(200, 507);
 			this->groupBox1->TabIndex = 0;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Menu";
 			// 
 			// openFileDialog1
 			// 
-			this->openFileDialog1->FileName = L"openFileDialog1";
+			this->openFileDialog1->FileName = "openFileDialog1";
+			openFileDialog1->InitialDirectory = ".\\";
+			openFileDialog1->Filter = "Stalker Objects (*.object)|*.object";
+			openFileDialog1->FilterIndex = 2;
+			openFileDialog1->RestoreDirectory = true;
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->filesToolStripMenuItem,
+					this->toolsToolStripMenuItem, this->helpToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(949, 24);
+			this->menuStrip1->TabIndex = 1;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// filesToolStripMenuItem
+			// 
+			this->filesToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->meshToolStripMenuItem });
+			this->filesToolStripMenuItem->Name = L"filesToolStripMenuItem";
+			this->filesToolStripMenuItem->Size = System::Drawing::Size(42, 20);
+			this->filesToolStripMenuItem->Text = L"Files";
+			// 
+			// meshToolStripMenuItem
+			// 
+			this->meshToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->loadToolStripMenuItem,
+					this->saveToolStripMenuItem, this->exportToolStripMenuItem
+			});
+			this->meshToolStripMenuItem->Name = L"meshToolStripMenuItem";
+			this->meshToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->meshToolStripMenuItem->Text = L"Mesh";
+			// 
+			// loadToolStripMenuItem
+			// 
+			this->loadToolStripMenuItem->Name = L"loadToolStripMenuItem";
+			this->loadToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->loadToolStripMenuItem->Text = L"Load";
+			this->loadToolStripMenuItem->Click += gcnew System::EventHandler(this, &MeshEdit::loadToolStripMenuItem_Click);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this->saveToolStripMenuItem->Name = L"saveToolStripMenuItem";
+			this->saveToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->saveToolStripMenuItem->Text = L"Save";
+			// 
+			// exportToolStripMenuItem
+			// 
+			this->exportToolStripMenuItem->Name = L"exportToolStripMenuItem";
+			this->exportToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->exportToolStripMenuItem->Text = L"Export";
+			// 
+			// toolsToolStripMenuItem
+			// 
+			this->toolsToolStripMenuItem->Name = L"toolsToolStripMenuItem";
+			this->toolsToolStripMenuItem->Size = System::Drawing::Size(48, 20);
+			this->toolsToolStripMenuItem->Text = L"Tools";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this->helpToolStripMenuItem->Name = L"helpToolStripMenuItem";
+			this->helpToolStripMenuItem->Size = System::Drawing::Size(44, 20);
+			this->helpToolStripMenuItem->Text = L"Help";
 			// 
 			// MeshEdit
 			// 
@@ -83,15 +164,24 @@ namespace ECore {
 				static_cast<System::Int32>(static_cast<System::Byte>(64)));
 			this->ClientSize = System::Drawing::Size(949, 536);
 			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->menuStrip1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MeshEdit";
 			this->Text = L"MeshEdit";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 	this->openFileDialog1->OpenFile();
+}
+private: System::Void loadToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) 
+{
+	openFileDialog1->ShowDialog();
 }
 };
 }
--- code/editors/xrMeshEditor/MeshEditor.cpp
+++ code/editors/xrMeshEditor/MeshEditor.cpp
@@ -1,10 +1,12 @@
 #include "MainForm.h"
 #include <Windows.h>
 
+[System::STAThreadAttribute]
 int APIENTRY WinMain(HINSTANCE hInsttance, HINSTANCE hPrevInstance, char* lpCmdLine, int nCmdShow)
 {
 	ECore::Application::EnableVisualStyles();
 	ECore::Application::SetCompatibleTextRenderingDefault(false);
 	ECore::Application::Run(gcnew ECore::MeshEdit);
 	return 0;
+//	System::STAThreadAttribute;
 }
