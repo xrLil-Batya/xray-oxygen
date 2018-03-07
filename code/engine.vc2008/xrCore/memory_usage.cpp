4af888e75122bed0929b27e2f9f5110afdf159de
sv3nk <sv3nk@yandex.ru>
2018-03-07 15:43:58 +0300

    * FIX: HeapWalk is very slow. Enable memory information in the log can be obtained using the "-memory_usage" key.

--- code/engine.vc2008/xrCore/memory_usage.cpp
+++ code/engine.vc2008/xrCore/memory_usage.cpp
@@ -32,43 +32,44 @@ XRCORE_API void log_vminfo	()
 
 u32	mem_usage_impl	(u32* pBlocksUsed, u32* pBlocksFree)
 {
-	static bool no_memory_usage = !!strstr(GetCommandLine(), "-no_memory_usage");
-	if (no_memory_usage)
-		return		0;
-
-	_HEAPINFO		hinfo;
-	int				heapstatus;
-	hinfo._pentry = nullptr;
-	size_t	total	= 0;
-	u32	blocks_free	= 0;
-	u32	blocks_used	= 0;
-	while((heapstatus = _heapwalk(&hinfo)) == _HEAPOK)
-	{ 
-		if (hinfo._useflag == _USEDENTRY)
-		{
-			total		+= hinfo._size;
-			blocks_used	+= 1;
-		} 
-		else blocks_free	+= 1;
-	}
-	if (pBlocksFree)	*pBlocksFree= 1024*(u32)blocks_free;
-	if (pBlocksUsed)	*pBlocksUsed= 1024*(u32)blocks_used;
-
-	switch( heapstatus )
+	if (!!strstr(GetCommandLine(), "-memory_usage"))
 	{
-	case _HEAPEMPTY:
-		break;
-	case _HEAPEND:
-		break;
-	case _HEAPBADPTR:
-		Msg			("bad pointer to heap");
-		break;
-	case _HEAPBADBEGIN:
-		Msg			("bad start of heap");
-		break;
-	case _HEAPBADNODE:
-		Msg			("bad node in heap");
-		break;
+		_HEAPINFO hinfo;
+		int heapstatus;
+		hinfo._pentry = nullptr;
+		size_t	total	= 0;
+		u32	blocks_free	= 0;
+		u32	blocks_used	= 0;
+		while((heapstatus = _heapwalk(&hinfo)) == _HEAPOK)
+		{ 
+			if (hinfo._useflag == _USEDENTRY)
+			{
+				total		+= hinfo._size;
+				blocks_used	+= 1;
+			} 
+			else blocks_free	+= 1;
+		}
+		if (pBlocksFree)	*pBlocksFree= 1024*(u32)blocks_free;
+		if (pBlocksUsed)	*pBlocksUsed= 1024*(u32)blocks_used;
+	
+		switch( heapstatus )
+		{
+		case _HEAPEMPTY:
+			break;
+		case _HEAPEND:
+			break;
+		case _HEAPBADPTR:
+			Msg			("bad pointer to heap");
+			break;
+		case _HEAPBADBEGIN:
+			Msg			("bad start of heap");
+			break;
+		case _HEAPBADNODE:
+			Msg			("bad node in heap");
+			break;
+		}
+		return (u32) total;
 	}
-	return (u32) total;
+	
+	return 0;
 }
