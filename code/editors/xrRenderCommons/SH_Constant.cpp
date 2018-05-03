26778f6358415b59914f77db3d095a39279a6081
TomPhantom <phantom1020@yandex.ru>
2018-05-03 09:37:29 +0400

    *English:         - Cleanup code of current files *Русский:         - Отформатирован код

--- code/editors/xrRenderCommons/SH_Constant.cpp
+++ code/editors/xrRenderCommons/SH_Constant.cpp
@@ -26,33 +26,35 @@ BOOL	AAA_test		()
 
 void	CConstant::Calculate()
 {
-//#GIPERION: RESTORE WHEN DEVICE GOT IN xrRenderCommons
+	//#GIPERION: RESTORE WHEN DEVICE GOT IN xrRenderCommons
 #if 0
 
-	if (dwFrame==RDEVICE->dwFrame)	return;
-	dwFrame		= RDEVICE->dwFrame;
-	if (modeProgrammable==dwMode)	return;
+	if (dwFrame == RDEVICE->dwFrame)
+		return;
 
-	float	t	= RDEVICE->fTimeGlobal;
-	set_float	(_R.Calculate(t),_G.Calculate(t),_B.Calculate(t),_A.Calculate(t));
+	dwFrame = RDEVICE->dwFrame;
+
+	if (modeProgrammable == dwMode)
+		return;
+
+	float	t = RDEVICE->fTimeGlobal;
+	set_float(_R.Calculate(t), _G.Calculate(t), _B.Calculate(t), _A.Calculate(t));
 #endif
-
 }
 
-void	CConstant::Load	(IReader* fs)
+void CConstant::Load(IReader* fs)
 {
-	dwMode		= modeWaveForm;
-	fs->r		(&_R,sizeof(WaveForm));
-	fs->r		(&_G,sizeof(WaveForm));
-	fs->r		(&_B,sizeof(WaveForm));
-	fs->r		(&_A,sizeof(WaveForm));
+	dwMode = modeWaveForm;
+	fs->r(&_R, sizeof(WaveForm));
+	fs->r(&_G, sizeof(WaveForm));
+	fs->r(&_B, sizeof(WaveForm));
+	fs->r(&_A, sizeof(WaveForm));
 }
 
-void	CConstant::Save	(IWriter* fs)
+void CConstant::Save(IWriter* fs)
 {
-	fs->w		(&_R,sizeof(WaveForm));
-	fs->w		(&_G,sizeof(WaveForm));
-	fs->w		(&_B,sizeof(WaveForm));
-	fs->w		(&_A,sizeof(WaveForm));
+	fs->w(&_R, sizeof(WaveForm));
+	fs->w(&_G, sizeof(WaveForm));
+	fs->w(&_B, sizeof(WaveForm));
+	fs->w(&_A, sizeof(WaveForm));
 }
-
