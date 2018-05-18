6f8408e92761cc9f55cab7a6dfb7dcc5bc81c4be
v2v3v4 <terran.erre@mail.ru>
2018-05-18 14:56:31 +0300

    * FIX: base materials fnife and ammo

--- code/engine.vc2008/xrGame/WeaponKnife.cpp
+++ code/engine.vc2008/xrGame/WeaponKnife.cpp
@@ -66,7 +66,8 @@ void CWeaponKnife::Load	(LPCSTR section)
 #endif
 	m_NextHitDivideFactor	=	pSettings->r_float(section, "splash_hit_divide_factor");
 
-	knife_material_idx =  GMLib.GetMaterialIdx(pSettings->r_string(section,"material"));
+	knife_material_idx =  GMLib.GetMaterialIdx(READ_IF_EXISTS(pSettings,r_string,section,"material","objects\\knife"));
+	
 }
 
 void CWeaponKnife::OnStateSwitch	(u32 S)
