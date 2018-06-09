880c2149a02f866bf203dfc0fda9abeb93347211
sv3nk <sv3nk@yandex.ru>
2018-06-09 13:58:21 +0300

    * FIX: Resolved  ps_r__Detail_height;

--- code/engine.vc2008/xrRender/xrRender/xrRender_console.cpp
+++ code/engine.vc2008/xrRender/xrRender/xrRender_console.cpp
@@ -140,6 +140,7 @@ float		ps_r__WallmarkTTL			= 50.f	;
 float		ps_r__WallmarkSHIFT			= 0.0001f;
 float		ps_r__WallmarkSHIFT_V		= 0.0001f;
 
+float		ps_r__Detail_height 		= 1.f;
 float		ps_r__GLOD_ssa_start		= 256.f	;
 float		ps_r__GLOD_ssa_end			=  64.f	;
 float		ps_r__LOD					=  0.75f	;
