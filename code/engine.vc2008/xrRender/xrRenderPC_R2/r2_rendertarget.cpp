e3d5c5a8e0a09c75ceb7406a28cf0fe49be42e14
sv3nk <sv3nk@yandex.ru>
2018-06-24 00:04:58 +0300

    * FIX: Lens Flare shader update.

--- code/engine.vc2008/xrRender/xrRenderPC_R2/r2_rendertarget.cpp
+++ code/engine.vc2008/xrRender/xrRenderPC_R2/r2_rendertarget.cpp
@@ -276,7 +276,7 @@ CRenderTarget::CRenderTarget		()
 	}
 
 	// FLARES
-	s_flare.create					("effects\\flare", "fx\\lenslare");
+	s_flare.create					("effects\\lensflare", "fx\\lenslare");
 	// RAIN DROPS
 	s_rain_drops.create             (b_rain_drops,  "r2\\sgm_rain_drops");
 	// OCCLUSION
