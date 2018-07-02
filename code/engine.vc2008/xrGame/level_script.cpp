f9138fcb3981680f905c0a6e50a98dd1b1dc63c4
Graff46 <macter46@yandex.ru>
2018-07-02 18:40:55 +0400

    Update level_script.cpp

--- code/engine.vc2008/xrGame/level_script.cpp
+++ code/engine.vc2008/xrGame/level_script.cpp
@@ -779,6 +779,16 @@ xrTime get_start_time()
 	return (xrTime(Level().GetStartGameTime()));
 }
 
+void supertest(LPCSTR sname)
+{
+	FS.curr_season = (char*) sname;
+}
+
+char* get_season()
+{
+	return (FS.curr_season);
+}
+
 #pragma optimize("s",on)
 void CLevel::script_register(lua_State *L)
 {
@@ -997,7 +1007,9 @@ void CLevel::script_register(lua_State *L)
 	    def("start_tutorial",		&start_tutorial),
 	    def("stop_tutorial",		&stop_tutorial),
 	    def("has_active_tutorial",	&has_active_tutotial),
-	    def("translate_string",		&translate_string)
+	    def("translate_string",		&translate_string),
+	    def("set_season",			&supertest),
+	    def("get_season",			&get_season)
 
 	];
 }
