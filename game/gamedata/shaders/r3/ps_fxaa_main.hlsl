bc6e6f087ccb81d55e650d617b2ac74d84ddb454
RainbowZerg <rei30e@gmail.com>
2018-07-02 17:02:04 +0300

    * ADD: Completely implemented FXAA (luma pass).

--- game/gamedata/shaders/r3/ps_fxaa_main.hlsl
+++ game/gamedata/shaders/r3/ps_fxaa_main.hlsl
@@ -1,36 +1,30 @@
 #include "common.h"
-#include "ps_fxaa.hlsl"
+#include "fxaa.h"
 
-Texture2D 	s_base0;
-
-struct	v2p 
+float4 main (p_screen I) : SV_Target
 {
-	float2 	Tex0	: TEXCOORD0;
-	float4 	HPos	: SV_Position;	 
-};
+    FxaaTex tex = { smp_rtlinear, s_image };
 
-float4 main (  v2p I ) : SV_Target
-{
-    float2 fxaaFrame = float2(1.0/screen_res.x, 1.0/screen_res.y);
-
-    FxaaTex tex = { smp_rtlinear, s_base0 };
-
-	return FxaaPixelShader(I.Tex0,
+	float4 img = FxaaPixelShader(
+			I.tc0,									// FxaaFloat2 pos,
             FxaaFloat4(0.0f, 0.0f, 0.0f, 0.0f),		// FxaaFloat4 fxaaConsolePosPos,
-            tex,							// FxaaTex tex,
-            tex,							// FxaaTex fxaaConsole360TexExpBiasNegOne,
-            tex,							// FxaaTex fxaaConsole360TexExpBiasNegTwo,
-            fxaaFrame,							// FxaaFloat2 fxaaQualityRcpFrame,
+            tex,									// FxaaTex tex,
+            tex,									// FxaaTex fxaaConsole360TexExpBiasNegOne,
+            tex,									// FxaaTex fxaaConsole360TexExpBiasNegTwo,
+            screen_res.zw,							// FxaaFloat2 fxaaQualityRcpFrame,
             FxaaFloat4(0.0f, 0.0f, 0.0f, 0.0f),		// FxaaFloat4 fxaaConsoleRcpFrameOpt,
             FxaaFloat4(0.0f, 0.0f, 0.0f, 0.0f),		// FxaaFloat4 fxaaConsoleRcpFrameOpt2,
             FxaaFloat4(0.0f, 0.0f, 0.0f, 0.0f),		// FxaaFloat4 fxaaConsole360RcpFrameOpt2,
             0.35f,									// FxaaFloat fxaaQualitySubpix,
             0.166f,									// FxaaFloat fxaaQualityEdgeThreshold,
-            0.0f,								// FxaaFloat fxaaQualityEdgeThresholdMin,
+            0.0f,//0.0625f,							// FxaaFloat fxaaQualityEdgeThresholdMin,
             0.0f,									// FxaaFloat fxaaConsoleEdgeSharpness,
             0.0f,									// FxaaFloat fxaaConsoleEdgeThreshold,
             0.0f,									// FxaaFloat fxaaConsoleEdgeThresholdMin,
-            FxaaFloat4(0.0f, 0.0f, 0.0f, 0.0f)		// FxaaFloat fxaaConsole360ConstDir,
+            FxaaFloat4(0.0f, 0.0f, 0.0f, 0.0f)		// FxaaFloat4 fxaaConsole360ConstDir,
             );
 	
+	img.w = 1.0f;
+	
+	return saturate(img);
 }
