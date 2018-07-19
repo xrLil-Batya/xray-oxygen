8d362de14e7271f5eab58213c48531641dc94bee
Borshig <comedytiger@gmail.com>
2018-07-19 21:54:41 +0300

    Fix: class_registrator
    Co-Authored-By: Mr. KRodin <krodinn@users.noreply.github.com>

--- code/engine.vc2008/xrServerEntities/script_engine.h
+++ code/engine.vc2008/xrServerEntities/script_engine.h
@@ -54,6 +54,7 @@ public:
 	virtual	void				unload						();
 	static	int					lua_panic					(lua_State *L);
 	static	int					lua_pcall_failed			(lua_State *L);
+	
 #ifdef DEBUG
 	static	void				lua_hook_call				(lua_State *L, lua_Debug *dbg);
 #endif // #ifdef DEBUG
