ab3bd19de0ffa453ab00cd751ff624e730e055d3
RainbowZerg <rei30e@gmail.com>
2018-10-07 03:15:31 +0300

    * FIX: Fixed background blurring in main menu on R3/R4.

--- game/gamedata/shaders/r3/ps_distort.hlsl
+++ game/gamedata/shaders/r3/ps_distort.hlsl
@@ -10,7 +10,7 @@ Texture2D s_distort;
 float4 main (v2p_TL I) : SV_Target
 {
 	float2 	distort	= s_distort.Load(int3(I.HPos.xy, 0), 0);
-	float2	offset	= (distort.xy - 0.5f)*def_distort;
+	float2	offset	= (distort.xy - 127.0f/255.0f)*def_distort;
 	float3	image 	= s_base.Sample(smp_rtlinear, I.Tex0 + offset);
 
 	return float4(image, 1.0f);
