2d7dfb539df4264d8dacb945e984cab0ccb0c6a9
sv3nk <sv3nk@yandex.ru>
2018-10-09 17:21:10 +0300

    * ADD: [Layuot Editor] Init xrCore on Layout Editor

--- code/editors/xrLayoutEditor/LayoutEditor.cpp
+++ code/editors/xrLayoutEditor/LayoutEditor.cpp
@@ -1,11 +1,11 @@
 //#include "MainForm.h"
-//#include "xrCore\xrCore.h"
+#include "..\..\engine.vc2008\xrCore\xrCore.h"
 #include <Windows.h>
 
 [System::STAThreadAttribute]
 int APIENTRY WinMain(HINSTANCE hInsttance, HINSTANCE hPrevInstance, char* lpCmdLine, int nCmdShow)
 {
-	//Core._initialize("Layout Editor", 0, true, "fs.ltx");
+	Core._initialize("Layout Editor", 0, true, "fsgame.ltx");
 	//ECore::Application::EnableVisualStyles();
 	//ECore::Application::SetCompatibleTextRenderingDefault(false);
 	//ECore::Application::Run(gcnew ECore::MeshEdit);
