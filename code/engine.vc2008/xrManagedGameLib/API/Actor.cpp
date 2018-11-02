78971de60064644c0ce16fff586395c7f7f2e94d
sv3nk <sv3nk@yandex.ru>
2018-11-02 15:37:50 +0300

    * ADD: [Spectre] Added virtual methods for Actor

--- code/engine.vc2008/xrManagedGameLib/API/Actor.cpp
+++ code/engine.vc2008/xrManagedGameLib/API/Actor.cpp
@@ -7,6 +7,68 @@ XRay::Actor::Actor(IntPtr InNativeObject)
 	CAST_TO_NATIVE_OBJECT(CActor, InNativeObject);
 
 	inventory = (Inventory^)Inventory::Create(InNativeObject, Inventory::typeid);
+
+	AddVirtualMethod("TradeStart");
+	AddVirtualMethod("TradeStop");
+	AddVirtualMethod("TradeSellBuyItem");
+	AddVirtualMethod("TradePerformOperation");
+
+	AddVirtualMethod("TraderGlobalAnimRequest");
+	AddVirtualMethod("TraderHeadAnimRequest");
+	AddVirtualMethod("TraderSoundEend");
+
+	AddVirtualMethod("ZoneEnter");
+	AddVirtualMethod("ZoneExit");
+
+	AddVirtualMethod("UseObject");
+}
+
+void XRay::Actor::UseObject(Object ^ pObj)
+{
+}
+
+void XRay::Actor::TradeStart()
+{
+
+}
+
+void XRay::Actor::TradeStop()
+{
+}
+
+void XRay::Actor::TradeSellBuyItem()
+{
+
+}
+
+void XRay::Actor::TradePerformOperation()
+{
+
+}
+
+void XRay::Actor::TraderGlobalAnimRequest()
+{
+
+}
+
+void XRay::Actor::TraderHeadAnimRequest()
+{
+
+}
+
+void XRay::Actor::TraderSoundEend()
+{
+
+}
+
+void XRay::Actor::ZoneEnter()
+{
+
+}
+
+void XRay::Actor::ZoneExit()
+{
+
 }
 
 XRay::Actor::Actor() : Actor(XRay::ClassRegistrator::GetFactoryTarget())
--- code/engine.vc2008/xrManagedGameLib/API/Actor.h
+++ code/engine.vc2008/xrManagedGameLib/API/Actor.h
@@ -11,13 +11,59 @@ namespace XRay
 {
 	public ref class Actor : public EntityAlive
 	{
+	internal:
+		CActor* pNativeObject;
+
 	public:
-		Actor();
+		Actor();;
 		Actor(IntPtr InNativeObject);
 
-		Inventory^ inventory;
+		//virtual void shedule_Update(u32 Interval);
+		virtual void UseObject(Object^ pObj);
 
-	private:
-		CActor* pNativeObject;
+		virtual void TradeStart();
+		virtual void TradeStop();
+		virtual void TradeSellBuyItem();
+		virtual void TradePerformOperation();
+
+		virtual void TraderGlobalAnimRequest();
+		virtual void TraderHeadAnimRequest();
+		virtual void TraderSoundEend();
+
+		virtual void ZoneEnter();
+		virtual void ZoneExit();
+
+		/*
+		virtual void level_border_exit();
+		virtual void level_border_enter();
+		virtual void death();
+		virtual void patrol_path_in_point();
+		virtual void inventory_info();
+		virtual void article_info();
+		virtual void hit();
+		virtual void sound();
+		virtual void action_removed();
+		virtual void action_movement();
+		virtual void action_watch();
+		virtual void action_animation();
+		virtual void action_sound();
+		virtual void action_particle();
+		virtual void action_object();
+		virtual void actor_sleep();
+		virtual void helicopter_on_point();
+		virtual void helicopter_on_hit();
+		virtual void on_item_take();
+		virtual void on_item_drop();
+		virtual void script_animation();
+		virtual void task_state();
+		virtual void take_item_from_box();
+		virtual void weapon_no_ammo();
+		virtual void on_action_press();
+		virtual void on_action_release();
+		virtual void on_action_hold();
+		virtual void map_location_added();
+		*/
+	public:
+		Inventory^ inventory;
 	};
 }
