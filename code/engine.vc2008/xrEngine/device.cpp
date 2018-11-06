c6627c47c5c777e98d1742e226ad9b3aac4b3e2b
Anton <camotank12345@gmail.com>
2018-11-06 19:37:28 +0300

    * NEW: Destroy duplicated process

--- code/engine.vc2008/xrEngine/device.cpp
+++ code/engine.vc2008/xrEngine/device.cpp
@@ -145,6 +145,7 @@ void 			mt_Thread	(void *ptr)
 		{
 			pit();
 		}
+
 		Device.seqParallel.clear();
 		Device.seqFrameMT.Process(rp_Frame);
 
@@ -160,35 +161,43 @@ void 			mt_Thread	(void *ptr)
 #include "igame_level.h"
 void CRenderDevice::PreCache	(u32 amount, bool b_draw_loadscreen, bool b_wait_user_input)
 {
-	if (m_pRender->GetForceGPU_REF()) 
-		amount = NULL; 
+	if (m_pRender->GetForceGPU_REF()) { amount = NULL; }
 
-	dwPrecacheFrame	= dwPrecacheTotal = amount;
-	if (amount && !precache_light && g_pGameLevel && g_loading_events.empty()) {
-		precache_light					= ::Render->light_create();
-		precache_light->set_shadow		(false);
-		precache_light->set_position	(vCameraPosition);
-		precache_light->set_color		(255,255,255);
-		precache_light->set_range		(5.0f);
-		precache_light->set_active		(true);
+	dwPrecacheFrame = dwPrecacheTotal = amount;
+	if (amount && !precache_light && g_pGameLevel && g_loading_events.empty()) 
+	{
+		precache_light = ::Render->light_create();
+		precache_light->set_shadow(false);
+		precache_light->set_position(vCameraPosition);
+		precache_light->set_color(255, 255, 255);
+		precache_light->set_range(5.0f);
+		precache_light->set_active(true);
 	}
 
-	if(amount && b_draw_loadscreen && load_screen_renderer.b_registered==false)
+	if (amount && b_draw_loadscreen && load_screen_renderer.b_registered == false)
 	{
-		load_screen_renderer.start	(b_wait_user_input);
+		load_screen_renderer.start(b_wait_user_input);
 	}
 }
 
-ENGINE_API xr_list<LOADING_EVENT>			g_loading_events;
+ENGINE_API xr_list<LOADING_EVENT> g_loading_events;
 
-void CRenderDevice::on_idle		()
+void CRenderDevice::on_idle()
 {
-	if (!b_is_Ready) { Sleep(100); return; }
+	if (!b_is_Ready) 
+	{
+		Sleep(100);
+		return; 
+	}
 
 	if (psDeviceFlags.test(rsStatistic))
+	{
 		g_bEnableStatGather = TRUE;
+	}
 	else
+	{
 		g_bEnableStatGather = FALSE;
+	}
 
 	if (!g_loading_events.empty())
 	{
@@ -240,8 +249,9 @@ void CRenderDevice::on_idle		()
 		if (Begin())
 		{
 			seqRender.Process(rp_Render);
-			if (psDeviceFlags.test(rsCameraPos) || psDeviceFlags.test(rsStatistic)
-			   || psDeviceFlags.test(rsDrawFPS) || psDeviceFlags.test(rsHWInfo) || !Statistic->errors.empty())
+			if (psDeviceFlags.test(rsCameraPos) || psDeviceFlags.test(rsStatistic)  || 
+				psDeviceFlags.test(rsDrawFPS)	|| psDeviceFlags.test(rsHWInfo)		|| 
+				!Statistic->errors.empty())
 			{
 				Statistic->Show();
 			}
@@ -268,15 +278,18 @@ void CRenderDevice::on_idle		()
 		seqFrameMT.Process(rp_Frame);
 	}
 
-	if (!b_is_Active)
-		Sleep(1);
+	if (!b_is_Active) 
+	{ 
+		Sleep(1); 
+	}
 }
 
 void CRenderDevice::ResizeProc(DWORD height, DWORD  width)
 {
-	std::string buf = "vid_mode " + std::to_string(width) + "x" + std::to_string(height);
-	Console->Execute(buf.c_str());
+	static char buf[128] = { NULL };
+	xr_sprintf(buf, "%s%d%s%d", "vid_mode ", width, "x", height);
 
+	Console->Execute(buf);
 	m_pRender->Reset(m_hWnd, dwWidth, dwHeight, fWidth_2, fHeight_2);
 }
 
