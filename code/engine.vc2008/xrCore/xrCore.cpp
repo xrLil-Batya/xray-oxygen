b8abe382356fc166b165b8fb0efb6dddc27214c1
sv3nk <sv3nk@yandex.ru>
2018-12-19 01:29:22 +0300

    * NEW: Check current xrCore.dll version.

--- code/engine.vc2008/xrCore/xrCore.cpp
+++ code/engine.vc2008/xrCore/xrCore.cpp
@@ -140,3 +140,11 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD ul_reason_for_call, LPVOID lpvRese
 	}
     return TRUE;
 }
+
+extern "C"
+{
+	__declspec(dllexport) char* GetCurrentHash()
+	{
+		return _HASH;
+	}
+}
