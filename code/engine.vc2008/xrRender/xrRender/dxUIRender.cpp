b7fd5c96c17f1663a4960cd8f4ea31fad4385839
sv3nk <sv3nk@yandex.ru>
2019-01-10 01:27:31 +0300

    * FIX: StateManager.OverrideScissoring no need on UIRender

--- code/engine.vc2008/xrRender/xrRender/dxUIRender.cpp
+++ code/engine.vc2008/xrRender/xrRender/dxUIRender.cpp
@@ -31,12 +31,7 @@ void dxUIRender::SetAlphaRef(int aref)
 
 void dxUIRender::SetScissor(Irect* rect)
 {
-#if (RENDER != R_R2)
 	RCache.set_Scissor(rect);
-	StateManager.OverrideScissoring( rect?true:false, TRUE );
-#else
-	RCache.set_Scissor(rect);
-#endif
 }
 
 void dxUIRender::GetActiveTextureResolution(Fvector2 &res)
