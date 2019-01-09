4f59180a74e02d6d01f1461c9c19593e81dbb59d
Giperion <dereline@hotmail.com>
2019-01-10 02:57:06 +0300

    Revert "* FIX: StateManager.OverrideScissoring no need on UIRender"
    This reverts commit b7fd5c96c17f1663a4960cd8f4ea31fad4385839.

--- code/engine.vc2008/xrRender/xrRender/dxUIRender.cpp
+++ code/engine.vc2008/xrRender/xrRender/dxUIRender.cpp
@@ -31,7 +31,12 @@ void dxUIRender::SetAlphaRef(int aref)
 
 void dxUIRender::SetScissor(Irect* rect)
 {
+#if (RENDER != R_R2)
 	RCache.set_Scissor(rect);
+	StateManager.OverrideScissoring( rect?true:false, TRUE );
+#else
+	RCache.set_Scissor(rect);
+#endif
 }
 
 void dxUIRender::GetActiveTextureResolution(Fvector2 &res)
