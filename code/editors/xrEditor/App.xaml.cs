f2aef7abaedf413381ff9c2784048fc1816029b7
sv3nk <sv3nk@yandex.ru>
2019-01-12 00:28:07 +0300

    * UPD: [To prev commit] MarshalAs replaced by @Giperionn function

--- code/editors/xrEditor/App.xaml.cs
+++ code/editors/xrEditor/App.xaml.cs
@@ -15,7 +15,7 @@ namespace xrEditor
     {
         protected override void OnStartup(System.Windows.StartupEventArgs e)
         {
-            SpectreEngine.xrCoreInit("Editor", "fsgame.ltx");
+            SpectreEngine.xrCoreInit("Editor", "fs.ltx");
             SpectreEngine.xrEngineInit();
             SpectreEngine.xrRenderInit();
             base.OnStartup(e);
