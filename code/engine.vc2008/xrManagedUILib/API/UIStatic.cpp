940f3ed1c9b05193fff9df7886facf862fb043b6
sv3nk <sv3nk@yandex.ru>
2019-01-15 01:08:54 +0300

    * ADD: [Spectre] [UI] Added CUIStatic and CUILines. * FIX: Fixed disable BOKE and DoF quality.

---
+++ code/engine.vc2008/xrManagedUILib/API/UIStatic.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "UIStatic.h"
+#include "../xrUICore/UIStatic.h"
+
+XRay::UIStatic::UIStatic()
+{
+	pNativeStatic = new CUIStatic();
+}
+
+XRay::UIStatic::~UIStatic()
+{
+	delete pNativeStatic;
+}
+
+XRay::UILines^ XRay::UIStatic::TextItemControl()
+{
+	return gcnew UILines(pNativeStatic->TextItemControl());
+}
+
+void XRay::UIStatic::InitTexture(::System::String^ Name)
+{
+	string128 AppNameStr = { 0 };
+	ConvertDotNetStringToAscii(Name, AppNameStr);
+	pNativeStatic->InitTexture(AppNameStr);
+}
+
+void XRay::UIStatic::StretchTexture::set(bool bUse)
+{
+	pNativeStatic->SetStretchTexture(bUse);
+}
