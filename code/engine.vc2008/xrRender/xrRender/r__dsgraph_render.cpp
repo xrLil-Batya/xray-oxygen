bba76125fe8f620111b96649d612bcb4eaee7e73
sv3nk <sv3nk@yandex.ru>
2019-01-20 23:23:53 +0300

    * FIX: @Borshig, @Giperion - WHF?! @@@

--- code/engine.vc2008/xrRender/xrRender/r__dsgraph_render.cpp
+++ code/engine.vc2008/xrRender/xrRender/r__dsgraph_render.cpp
@@ -509,10 +509,8 @@ void R_dsgraph_structure::r_dsgraph_render_hud_ui()
 	Device.mFullTransform.mul(Device.mProject, Device.mView);
 	RCache.set_xform_project(Device.mProject);
 
-#if	RENDER!=R_R1
 	// Targets, use accumulator for temporary storage
 	const ref_rt	rt_null;
-<<<<<<< Updated upstream
 	RCache.set_RT(nullptr,	1);
 	RCache.set_RT(nullptr,	2);
 
@@ -531,30 +529,6 @@ void R_dsgraph_structure::r_dsgraph_render_hud_ui()
 	rmNear						();
 	g_hud->RenderActiveItemUI	();
 	rmNormal					();
-=======
-	RCache.set_RT(0, 1);
-	RCache.set_RT(0, 2);
-#if	(RENDER==R_R3) || (RENDER==R_R4)
-	if (!RImplementation.o.dx10_msaa)
-	{
-		if (RImplementation.o.albedo_wo)	RImplementation.Target->u_setrt(RImplementation.Target->rt_Accumulator, rt_null, rt_null, HW.pBaseZB);
-		else								RImplementation.Target->u_setrt(RImplementation.Target->rt_Color, rt_null, rt_null, HW.pBaseZB);
-	}
-	else
-	{
-		if (RImplementation.o.albedo_wo)	RImplementation.Target->u_setrt(RImplementation.Target->rt_Accumulator, rt_null, rt_null, RImplementation.Target->rt_MSAADepth->pZRT);
-		else								RImplementation.Target->u_setrt(RImplementation.Target->rt_Color, rt_null, rt_null, RImplementation.Target->rt_MSAADepth->pZRT);
-	}
-#else // (RENDER==R_R3) || (RENDER==R_R4)
-	if (RImplementation.o.albedo_wo)	RImplementation.Target->u_setrt(RImplementation.Target->rt_Accumulator, rt_null, rt_null, HW.pBaseZB);
-	else								RImplementation.Target->u_setrt(RImplementation.Target->rt_Color, rt_null, rt_null, HW.pBaseZB);
-#endif // (RENDER==R_R3) || (RENDER==R_R4)
-#endif // RENDER!=R_R1
-
-	rmNear();
-	g_hud->RenderActiveItemUI();
-	rmNormal();
->>>>>>> Stashed changes
 
 	// Restore projection
 	Device.mProject = Pold;
@@ -575,7 +549,6 @@ void	R_dsgraph_structure::r_dsgraph_render_sorted()
 // strict-sorted render
 void	R_dsgraph_structure::r_dsgraph_render_emissive()
 {
-#if	RENDER!=R_R1
 	// Sorted (back to front)
 	mapEmissive.traverseLR(sorted_L1);
 	mapEmissive.clear();
@@ -607,18 +580,15 @@ void	R_dsgraph_structure::r_dsgraph_render_emissive()
 	Device.mProject = Pold;
 	Device.mFullTransform = FTold;
 	RCache.set_xform_project(Device.mProject);
-#endif
 }
 
 //////////////////////////////////////////////////////////////////////////
 // strict-sorted render
 void	R_dsgraph_structure::r_dsgraph_render_wmarks()
 {
-#if	RENDER!=R_R1
 	// Sorted (back to front)
 	mapWmark.traverseLR(sorted_L1);
 	mapWmark.clear();
-#endif
 }
 
 //////////////////////////////////////////////////////////////////////////
