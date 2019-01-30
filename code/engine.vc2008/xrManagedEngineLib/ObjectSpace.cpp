956f1cf567a59a74c7c9b6eaa8c068ff5aea98c5
phantom1020 <phantom1020@yandex.ru>
2019-01-30 14:17:25 +0300

    * ADD: maybe it's ok

--- code/engine.vc2008/xrManagedEngineLib/ObjectSpace.cpp
+++ code/engine.vc2008/xrManagedEngineLib/ObjectSpace.cpp
@@ -30,12 +30,68 @@ namespace XRay
 
 	}
 
-	bool ObjectSpace::RayPick()
+	XRay::Object^ ObjectSpace::GetHittedObject()
 	{
-		// @ just reference to R;
+		// @ How to use it??? 
+		return nullptr;
+	}
+
+	bool ObjectSpace::RayPick(Vector3^ start, Vector3^ dir, float range, RQ_TraceType type, XRay::Object^ ignore_object)
+	{
+		Fvector s;
+		s.x = start->X;
+		s.y = start->Y;
+		s.z = start->Z;
+
+		Fvector d;
+		d.x = dir->X;
+		d.y = dir->Y;
+		d.z = dir->Z;
+
+		collide::rq_target b = (collide::rq_target)type;
+
+		collide::rq_result object_status;
+		CObject* obj = (CObject*)ignore_object->GetNativeObject().ToPointer();  
+		bool status = pNativeObject->RayPick(s, d, range, b, object_status, obj);
+		if (status)
+		{
+			if (object_status.O)
+			{
+				current_object = object_status.O;
+			}
+			else
+			{
+				current_object = nullptr;
+			}
+		}
+
+		return status;
+	}
+
+	
+	bool ObjectSpace::RayTest(Vector3^ start, Vector3^ dir, float range, RQ_TraceType type, XRay::Object^ ignore_object)
+	{
+		Fvector s; 
+		s.x = start->X;
+		s.y = start->Y;
+		s.z = start->Z;
+
+		Fvector d;
+		d.x = start->X;
+		d.y = start->Y;
+		d.z = start->Z;
+
+		collide::rq_target b = (collide::rq_target)type;
+		CObject* obj = (CObject*)ignore_object->GetNativeObject().ToPointer();
+		bool status = pNativeObject->RayTest(s, d, range, b, nullptr, obj);
+		
+		return status;
+	}
+
+	bool ObjectSpace::RayPickFromActor(RQ_TraceType type)
+	{
  
 		return false;
 	}
-
 }
 
--- code/engine.vc2008/xrManagedEngineLib/ObjectSpace.h
+++ code/engine.vc2008/xrManagedEngineLib/ObjectSpace.h
@@ -7,14 +7,27 @@ using namespace System;
 
 namespace XRay
 {
+	public enum class RQ_TraceType
+	{
+		None = (0),
+		Object = (1 << 0),
+		Static = (1 << 1),
+		Shape = (1 << 2),
+		Obstacle = (1 << 3),
+		Both = (Object | Static),
+		rqtDyn = (Object | Shape | Obstacle)
+	};
+
 	public ref class ObjectSpace
 	{
 	public:
 		ObjectSpace();
 		ObjectSpace(IntPtr InNativeObject);
 		String^ GetHittedObjectName();
-	//	XRay::Object^ GetHittedObject(); @ Indeed, but I can't implement that method until . . . 
-		bool RayPick();
+		XRay::Object^ GetHittedObject(); 
+		bool RayPick(Vector3^, Vector3^, float, RQ_TraceType, XRay::Object^);
+		bool RayTest(Vector3^, Vector3^, float, RQ_TraceType, XRay::Object^);
+		bool RayPickFromActor(RQ_TraceType type);
 	private:
 		CObject* current_object;
 		CObjectSpace* pNativeObject;
