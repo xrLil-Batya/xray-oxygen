7ca7cfc29a467f804301ade92d6998d43d0c0334
sv3nk <sv3nk@yandex.ru>
2019-04-06 00:49:59 +0300

    * EXP: Exported CPatrolPathStorage class for xrManagedGameLib.dll * NEW: Added color light for lmodel. * DEL: Cleanup lmodel.h: plight_infinity2

--- game/gamedata/shaders/r3/lmodel.h
+++ game/gamedata/shaders/r3/lmodel.h
@@ -18,35 +18,21 @@ float4 plight_infinity( float m, float3 pnt, float3 normal, float3 light_directi
 	
 	//s_material.Sample( smp_material, float3( dot(L,N), dot(H,N), m ) ).xxxy;		// sample material
 }
-/*
-float plight_infinity2( float m, float3 pnt, float3 normal, float3 light_direction )
-{
-  	float3 N		= normal;							// normal 
-  	float3 V 	= -normalize		(pnt);		// vector2eye
-  	float3 L 	= -light_direction;					// vector2light
- 	float3 H		= normalize			(L+V);			// float-angle-vector 
-	float3 R     = reflect         	(-V,N);
-	float 	s	= saturate(dot(L,R));
-			s	= saturate(dot(H,N));
-	float 	f 	= saturate(dot(-V,R));
-			s  *= f;
-	float4	r	= tex3D 			(s_material,	float3( dot(L,N), s, m ) );	// sample material
-			r.w	= pow(saturate(s),4);
-  	return	r	;
-}
-*/
 
-float4 plight_local( float m, float3 pnt, float3 normal, float3 light_position, float light_range_rsq, out float rsqr )
+float4 plight_local(float m, float3 pnt, float3 normal, float3 light_position, float light_range_rsq, out float rsqr)
 {
-  	float3 N		= normal;							// normal 
-  	float3 L2P 	= pnt-light_position;                         		// light2point 
-  	float3 V 	= -normalize	(pnt);					// vector2eye
-  	float3 L 	= -normalize	((float3)L2P);					// vector2light
-  	float3 H		= normalize	(L+V);						// float-angle-vector
-		rsqr	= dot		(L2P,L2P);					// distance 2 light (squared)
-  	float  att 	= saturate	(1 - rsqr*light_range_rsq);			// q-linear attenuate
-	float4 light	= s_material.Sample( smp_material, float3( dot(L,N), dot(H,N), m ) ).xxxy;		// sample material
-  	return att*light;
+
+	float3 N		= normal;										// normal 
+	float3 L2P 	= pnt - light_position;                         	// light2point 
+	float3 V 		= -normalize	(pnt);							// vector2eye
+	float3 L 		= -normalize	((float3)L2P);					// vector2light
+	float3 H		= normalize	(L+V);								// half-angle-vector
+		   rsqr		= dot		(L2P,L2P);							// distance 2 light (squared)
+	float  att 		= saturate	(1 - rsqr*light_range_rsq);			// q-linear attenuate
+	
+	float4 light	= tex3D		(s_material, float3( dot(L,N), dot(H,N), m ) ); 
+	
+  return float4(att * light.xxx,0) + att*float4(light.www * (Ldynamic_color.xyz * Ldynamic_color.xyz),light.w);
 }
 
 //	TODO: DX10: Remove path without blending
