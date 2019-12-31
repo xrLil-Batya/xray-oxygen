c51786b496e9ac4bd187edca1817c07eba4c94d3
ForserX <sv3nk@yandex.ru>
2019-12-31 10:27:16 +0300

    * LANG: Removed TMakeSingleton.

--- code/engine.vc2008/xrCore/xrLang.h
+++ code/engine.vc2008/xrCore/xrLang.h
@@ -57,13 +57,3 @@ private:
     TNonCopyable(const TNonCopyable&) = delete;
     TNonCopyable& operator=(const TNonCopyable&) = delete;
 };
-
-#define TMakeSingleton(T)				\
-	public:                             \
-	T(T const&) = delete;               \
-	void operator=(T const&) = delete;  \
-	static T& GetInstance()             \
-	{                                   \
-		static T Instance;              \
-		return Instance;                \
-	}
